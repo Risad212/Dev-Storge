3f21efd076528996c93440f235c6f14625a89f05
Risad212 <hafez.risad@gmail.com>
2023-03-13 16:43:55 +0600

    start wish list

---
+++ src/Pages/Wishlist.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Wishlist = () => {
+    return (
+        <div className='wishlist'>
+           <div className="container">
+               <div className="row">
+                  <div className="col-lg-8">
+                     
+                  </div>
+               </div>
+           </div>
+        </div>
+    );
+};
+
+export default Wishlist;
--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -11,6 +11,7 @@ import NotFound from '../NotFound/NotFound';
 import ProductDetails from '../productDetails/ProductDetails';
 import Contact from '../../Pages/Contact';
 import BlogDetails from '../BlogDetails/BlogDetails';
+import Wishlist from '../../Pages/Wishlist';
 
 const Router = () => {
     return (
@@ -24,6 +25,7 @@ const Router = () => {
               <Route path='/blog' element={<Blog />} />
               <Route path='/blog/:title' element={<BlogDetails />} />
               <Route path='/contact' element={<Contact />} />
+              <Route path='/wishlist' element={<Wishlist />} />
               <Route path='/product/category/:catname' element={<CategoryDetails />} />
               <Route path='*' element={<NotFound />} />
            </Routes>
