b160a90f9071f54a1e7e62b7347de3fd3188d8e6
Risad212 <hafez.risad@gmail.com>
2023-03-14 14:40:36 +0600

    done

--- src/components/WishProduct/WishProduct.jsx
+++ src/components/WishProduct/WishProduct.jsx
@@ -9,9 +9,24 @@ const WishProduct = () => {
                 <table class="table table-bordered">
                    <thead>
                        <tr>
-                          <th>Hello</th>
+                         <th>Product Name</th>
+                         <th>Product Image</th>
+                         <th>Unit Price</th>
+                         <th>Stock Status</th>
+                         <th>Add to Cart</th>
+                         <th>Delete</th>
                        </tr>
                    </thead>
+                   <tbody>
+                      <tr>
+                         <td>T-shirt Black</td>
+                         <td><img src="https://demo.themefreesia.com/shoppingcart/wp-content/uploads/sites/47/2019/03/woman-female-standing-fashion-clothing-outerwear-527825-pxhere.com_-300x300.jpg" class="img-fluid" alt="" /></td>
+                         <td>$54</td>
+                         <td>in a stock</td>
+                         <td><button>add to cart</button></td>
+                         <td><i class="fa-solid fa-circle-xmark"></i></td>
+                      </tr>
+                   </tbody>
               </table>
             </div>
         </>
